1121889cf818628d612f59b0f1d0e4274423d8e8
ramkrushnaw04 <ramkrushnaw04@gmail.com>
2025-06-14 12:39:41 +0530

    backend

---
+++ backend/src/server.js
@@ -0,0 +1,44 @@
+// modules
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors')
+const mongoose = require('mongoose');
+
+// api functions
+const query = require('./routes/query.js');
+const getSearchHistory = require('./routes/searchHistory.js');
+const proposalFromId = require('./routes/proposalFromId.js')
+
+// config
+const app = express();
+const PORT = 3000;
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+const databaseUrl = process.env.DATABASE_URL || 'mongodb://localhost:27017/PlanForage';
+
+// db
+mongoose.connect(databaseUrl)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// middlewares
+app.use(express.json());
+app.use(cors({
+  origin: allowedOrigins, 
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+}));
+
+
+// routes
+app.get('/', (req, res) => {
+  res.send('Backend running...');
+});
+app.post('/query', query)
+app.get('/search-history/:uid', getSearchHistory)
+app.get('/fetch-proposal/:id', proposalFromId)
+
+
+// app
+app.listen(PORT, () => {
+  console.log(`Server running on port...`);
+});
