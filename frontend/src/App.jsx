45bb4b770e24250750cc792daa0e8a818b51a43a
ramkrushnaw04 <ramkrushnaw04@gmail.com>
2025-06-22 18:42:48 +0530

    updated Login import error for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./components/Home";
-import Login from './components/Login';
+import Login from "./components/Login";
 
 const App = () => {
   return (
